b02b3fedc8e54afc0684d4312b972ddfd09b228b
Filip Heider <filip.heider@email.cz>
2025-09-21 12:11:39 +0200

    commit cuda

---
+++ cuda/cuda1/main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <cuda_device_runtime_api.h>
+#include <cuda_runtime.h>
+
+// Prototype of function from .cu file
+void cu_run_cuda( dim3 t_grid_size, dim3 t_block_size );
+
+int main()
+{
+    // Function calling
+    dim3 l_grid_size( 3, 2 ), l_block_size( 2, 3 );
+
+    cu_run_cuda( l_grid_size, l_block_size );
+
+    return 0;
+}
